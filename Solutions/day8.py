46d24e886c9c824bf45e59be06854bb4c2bf46cd
Daniel <dm730@exeter.ac.uk>
2023-12-11 09:05:39 +0000

    Day 8 finished

--- Solutions/day8.py
+++ Solutions/day8.py
@@ -1,31 +1,32 @@
 from pprint import pprint
 import re
-# with open("Inputs/day8.txt") as f:
-#     instructions = f.readline()
-#     instructions = [x for x in instructions if x != "\n"]
-#     f.readline()
+import math as Math
+with open("Inputs/day8.txt") as f:
+    instructions = f.readline()
+    instructions = [x for x in instructions if x != "\n"]
+    f.readline()
 
-#     lines = f.readlines()
-#     nodes = []
-#     for line in lines:
-#         node = (re.findall("[a-zA-Z]+", line))
-#         nodes.append(node)
+    lines = f.readlines()
+    nodes = []
+    for line in lines:
+        node = (re.findall("[a-zA-Z]+", line))
+        nodes.append(node)
 
-#     currentDest = "AAA"
-#     index = -1
-#     numMoves = 0
-#     while currentDest != "ZZZ":
-#         index = (index + 1) % len(instructions)
-#         numMoves+=1
-#         for i in range(len(nodes)):
-#             if nodes[i][0] == currentDest:
-#                 if (instructions[index] == "L"):
-#                     currentDest = nodes[i][1]
-#                 else:
-#                     currentDest = nodes[i][2]
-#                 break
+    currentDest = "AAA"
+    index = -1
+    numMoves = 0
+    while currentDest != "ZZZ":
+        index = (index + 1) % len(instructions)
+        numMoves+=1
+        for i in range(len(nodes)):
+            if nodes[i][0] == currentDest:
+                if (instructions[index] == "L"):
+                    currentDest = nodes[i][1]
+                else:
+                    currentDest = nodes[i][2]
+                break
     
-#     print(numMoves)
+    print(numMoves)
 
 
 with open("Inputs/day8.txt") as f:
@@ -46,29 +47,22 @@ with open("Inputs/day8.txt") as f:
     pprint(currentDests)
     index = -1
     numMoves = 0
-    loop = True
-    while (loop):
-        numMoves+=1
-        index = (index + 1) % len(instructions)
-        for x in range(len(currentDests)):
-            for i in range(len(nodes)):
-                if nodes[i][0] == currentDests[x]:
-                    if (instructions[index] == "L"):
-                        currentDests[x] = nodes[i][1]
-                    else:
-                        currentDests[x] = nodes[i][2]
-                    break
-        
-        pprint(currentDests)
+    numMovesList = []
 
-        allZ = True
-        for x in range(len(currentDests)):
-            if (currentDests[x][-1] != 'Z'):
-                allZ = False
-                break
-        
-        if (allZ):
-            print("FINISHED")
-            loop = False
-            break
-    print(numMoves)
+    for i in range(len(currentDests)):
+        currentDest = currentDests[i]
+        while currentDest[-1] != "Z":
+            index = (index + 1) % len(instructions)
+            numMoves+=1
+            for i in range(len(nodes)):
+                if nodes[i][0] == currentDest:
+                    if (instructions[index] == "L"):
+                        currentDest = nodes[i][1]
+                    else:
+                        currentDest = nodes[i][2]
+                    break
+        numMovesList.append(numMoves)
+        numMoves = 0
+        index = -1
+    
+    print(Math.lcm(*numMovesList))
