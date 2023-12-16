ec03dc604e7f90c36d9dca6e6f7141d9bb48f16d
Daniel Moulton <dan.moulton@btinternet.com>
2023-12-16 00:42:00 +0000

    Finished part 2

--- Solutions/day15.py
+++ Solutions/day15.py
@@ -10,7 +10,6 @@ def calcStep(step):
 
 with open("Inputs/day15.txt", 'r') as file:
     input = file.readline().strip().split(',')
-    pprint(input)
     stepDict = {}
     sum = 0
     for step in input:
@@ -18,14 +17,38 @@ with open("Inputs/day15.txt", 'r') as file:
             stepDict[step] = calcStep(step)
         sum += stepDict[step]
     print(sum)
-    
+
 with open("Inputs/day15.txt", 'r') as file:
     input = file.readline().strip().split(',')
-    pprint(input)
     stepDict = {}
     sum = 0
+    for i in range(256):
+        stepDict[i] = []
     for step in input:
-        if step not in stepDict:
-            stepDict[step] = calcStep(step)
-        sum += stepDict[step]
+        print(step)
+        if '-' in step:
+            # If we have a -, we want to hash the rest of the step, go to that box
+            # and if a lens w matching label is there, remove it and shift rest left
+            # Otherwise do nothing 
+            hash = calcStep(step[:-1])
+            contents = stepDict[hash]
+            for box in contents:
+                if box[0] == ''.join(step[:-1]):
+                    contents.remove(box)
+                    break
+        else:
+            focalLens = step[-1]
+            hash = calcStep(step[:-2])
+            contents = stepDict[hash]
+            for box in contents:
+                if box[0] == ''.join(step[:-2]):
+                    box[1]=focalLens
+                    break
+            else:
+                contents.append([step[:-2], focalLens])
+            pass
+    for key in stepDict:
+        if stepDict[key] != []:
+            for i, box in enumerate(stepDict[key]):
+                sum += (key+1) * (i+1) * int(box[1])
     print(sum)
